cac15c314801aa3ddf0154ed38c8f5ce147b91ee
V <spitsynvova@gmail.com>
2026-05-29 19:54:26 +0300

    f(?)

--- task1.cpp
+++ task1.cpp
@@ -1,43 +1,63 @@
 #include "task1.h"
 //
 #include <iostream>
+#include <vector>
+
 //
 void GetCoeficients(int n, const std::vector<double>& x, const std::vector<double>& f,
                     double a, double b, std::vector<double>& coef, int deg) {
-    // lambda вычисление многочлена Чебышева
-    auto chebyshevT = [](int k, double t) {
-        if (k == 0) return 1.0;
-        if (k == 1) return t;
-        double t0 = 1.0, t1 = t;
-        for (int i = 2; i <= k; ++i) {
-            double t2 = 2.0 * t * t1 - t0;
-            t0 = t1;
-            t1 = t2;
+
+    double gij = 0.0, val = 0.0;
+    double zj=0.0;
+    std::vector<double> gk0(n+2,0);
+    std::vector<double> gk1(n+2,0);
+    std::vector<double> z(n+2,0);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0)
+            {
+                gij = f[j];
+                gk0[j] = gij;
+            }            
+            else if(i == 1)
+            {
+                double t = (2.0 * x[j] - (a + b)) / (b - a);   // t ∈ [-1,1]
+                zj = t;
+                gij = zj*f[j];
+                gk1[j] = gij;
+                z[j] = 2.0*zj;
+            }
+            else
+            {
+                if(i%2 == 0)
+                {
+                    gij = z[j]*gk1[j] - gk0[j];
+                    gk0[j] = gij;
+                }
+                else
+                {
+                    gij = z[j]*gk0[j] - gk1[j];
+                    gk1[j] = gij;
+                }
+            }
+            val += gij;
+
         }
-        return t1;
-    };
-
-    std::vector<double> t(n);
-    for (int i = 0; i < n; ++i) {
-        t[i] = (2.0 * x[i] - (a + b)) / (b - a);
-    }
-
-    std::fill(coef.begin(), coef.end(), 0.0);
-
-    for (int j = 0; j < n; ++j) {
-        for (int k = 0; k < deg; ++k) {
-            coef[k] += f[j] * chebyshevT(k, t[j]);
+       
+        if (i == 0)
+        {
+            coef[i] = val/n;
         }
-    }
-
-    // Масштабирование
-    for (int k = 0; k < deg; ++k) {
-        if (k == 0) coef[k] /= n;
-        else coef[k] *= 2.0 / n;
+        else
+        {
+            coef[i] = 2.0*val/n;
+        }        
+        
+        val = 0;
     }
 }
-
-    
 double GetValue(double point, double a, double b, int n,const std::vector<double>& coef1,int deg ){
     double z=2.0*(2.0*point-(b+a))/(b-a);
     double value=0;
